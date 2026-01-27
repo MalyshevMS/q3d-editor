0199c25a93e80d768b76d7e0d44880c7e0bf8efb
MalyshevMS <matvey.malyshev29@gmail.com>
2026-01-27 22:13:46 +0300

    минимально рабочий пример для создания пустого окна

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <q3d/window/window.hpp>
 
 int main(int argc, char const *argv[]) {
-    test();
+    q3d::Window window {"q3d editor", {800, 600}};
+
+    while (window.isOpen()) {
+        window.update();
+    }
+
+    q3d::Window::terminate();
     return 0;
 }
