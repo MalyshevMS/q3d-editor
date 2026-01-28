d844bbb1a73935ca0e30960854d4e5bd79027ad6
MalyshevMS <matvey.malyshev29@gmail.com>
2026-01-28 20:38:54 +0300

    квадрат

--- src/main.cpp
+++ src/main.cpp
@@ -2,53 +2,56 @@
 #include <q3d/window/window.hpp>
 #include <q3d/res/resources.hpp>
 #include <q3d/gl/shader.hpp>
+#include <q3d/gl/vao.hpp>
 
 int main(int argc, char const *argv[]) {
     q3d::Window window {"q3d editor", {800, 600}};
     auto res = q3d::Resources::getInstance(argv[0]);
 
     const GLfloat verticies[] = {
-//       X      Y      Z            R      G      B
-        0.5f, -0.5f,  0.0f,        1.f,   0.f,   0.f,
-       -0.5f, -0.5f,  0.0f,        0.f,   1.f,   0.f,
-        0.0f,  0.5f,  0.0f,        0.f,   0.f,   1.f,
+//       X      Y      Z            R      G      B         U    V
+       -1.0f, -1.0f,  0.0f,        1.f,   0.f,   0.f,      0.f, 0.f,
+        1.0f, -1.0f,  0.0f,        0.f,   1.f,   0.f,      0.f, 1.f,
+        1.0f,  1.0f,  0.0f,        0.f,   0.f,   1.f,      1.f, 1.f,
+       -1.0f,  1.0f,  0.0f,        1.f,   1.f,   1.f,      1.f, 0.f,
     };
 
-    GLuint vao, vbo;
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
+    const GLuint ind[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
 
-    glBindVertexArray(vao);
+    // Scope for the variables, that must be destroyed before OpenGL Context closed // TODO: Encapsulate
+    { 
+        q3d::gl::Vao vao;
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+        q3d::gl::buffer::Layout l_xyz_rgb = {
+            q3d::gl::buffer::DataType::float3,
+            q3d::gl::buffer::DataType::float3,
+            q3d::gl::buffer::DataType::float2,
+        };
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0x0);
-    glEnableVertexAttribArray(0);
+        q3d::gl::Vbo vbo(verticies, sizeof(verticies), l_xyz_rgb);
+        q3d::gl::Ibo ibo(ind, sizeof(ind) / sizeof(GLuint));
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
+        vao.addVbo(vbo);
+        vao.setIbo(ibo);
 
-    glBindVertexArray(0);
+        q3d::gl::Shader shader;
+        shader.attach(res->readFile("res/main.vert"), GL_VERTEX_SHADER);
+        shader.attach(res->readFile("res/main.frag"), GL_FRAGMENT_SHADER);
+        shader.link();
 
-    auto shader = new q3d::gl::Shader;
-    shader->attach(res->readFile("res/main.vert"), GL_VERTEX_SHADER);
-    shader->attach(res->readFile("res/main.frag"), GL_FRAGMENT_SHADER);
-    shader->link();
+        glClearColor(0.f, 0.f, 0.f, 1.f);
+        while (window.isOpen()) {
+            glClear(GL_COLOR_BUFFER_BIT);
+            shader.use();
 
-    glClearColor(0.f, 0.f, 0.f, 1.f);
-    while (window.isOpen()) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        shader->use();
-
-        glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
-        shader->unuse();
-        window.update();
-    }
-    
-    delete shader;
-    q3d::Window::terminate();
+            vao.draw();
+            
+            shader.unuse();
+            window.update();
+        }
+    } q3d::Window::terminate();
     return 0;
 }
