b82f24fc01bf0976e6c1f8ad50349a775b3bebdd
MalyshevMS <matvey.malyshev29@gmail.com>
2026-02-21 13:34:52 +0300

    сделал файл с настройками, сделал выращение плоскости

--- src/app.cpp
+++ src/app.cpp
@@ -1,15 +1,14 @@
 #include "app.hpp"
 #include <q3d/gl/gl.hpp>
-#include <q3d/gl/vao.hpp>
 #include <q3d/core/active_camera.hpp>
 #include <q3d/2d/plane.hpp>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include "config.txx"
 
 Application::Application(std::string_view argv0)
  : window("q3d editor", { 1280, 720 }), cam(window.getAspectRatio(), 90.f), res(nullptr) {
     res = q3d::Resources::getInstance(argv0);
-    q3d::core::ActiveCamera::getInstance(std::shared_ptr<q3d::core::Camera>(&cam));
+    q3d::core::ActiveCamera::getInstance(q3d::ptr<q3d::core::Camera>(&cam));
 }
 
 void Application::run() {
@@ -17,14 +16,10 @@ void Application::run() {
     // Scope for the variables, that must be destroyed before OpenGL Context closed
     {
         auto texture = res->loadTexture("tex1", "res/texture.png");
-
         texture->setFilter(q3d::gl::Texture::Filter::NearestMMNearest, q3d::gl::Texture::Filter::Nearest);
         texture->uv = glm::vec2(2, 2);
 
-        auto shader = std::make_shared<q3d::gl::Shader>();
-        shader->attach(res->readFile("res/main.vert"), q3d::gl::Shader::Type::Vertex);
-        shader->attach(res->readFile("res/main.frag"), q3d::gl::Shader::Type::Fragment);
-        shader->link();
+        auto shader = res->loadShader("main", "res/main.vert", "res/main.frag");
 
         q3d::q2d::Plane plane(shader, {}, texture);
 
@@ -34,13 +29,13 @@ void Application::run() {
         while (window.isOpen()) {
             // CPU (math)
 
-            const float dt = window.getDeltaTime();
-            const glm::vec2 dm = window.getDeltaMouse();
-            const float cameraSpeed = 10.f;
-            const float sensetivity = 5.f;
-            const float cameraMove = cameraSpeed * dt;
-            glm::vec3 cameraMoveDelta = glm::vec3(0.f);
-            glm::vec3 cameraRotateDelta = glm::vec3(0.f);
+            const auto dt = window.getDeltaTime();
+            const auto dm = window.getDeltaMouse();
+            const float cameraMove = cfg::cameraSpeed * dt;
+            auto cameraMoveDelta = glm::vec3(0.f);
+            auto cameraRotateDelta = glm::vec3(0.f);
+
+            plane.transform.rotation.z += cfg::rotSpeed * dt;
 
             if (window.isKeyPressed(q3d::key::W)) {
                 cameraMoveDelta.z += cameraMove;
@@ -63,14 +58,14 @@ void Application::run() {
 
             if (window.isMouseButtonPressed(q3d::button::RIGHT)) {
                 window.hideCursor();
-                cameraRotateDelta.x -= dm.y * sensetivity * dt;
-                cameraRotateDelta.y -= dm.x * sensetivity * dt;
+                cameraRotateDelta.x -= dm.y * cfg::cameraSensetivity * dt;
+                cameraRotateDelta.y -= dm.x * cfg::cameraSensetivity * dt;
             } else window.showCursor();
 
             cam.moveRotate(cameraMoveDelta, cameraRotateDelta);
 
             // GPU
-// 
+ 
             q3d::gl::clear();
             
             plane.draw();
---
+++ src/config.txx
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace cfg {
+    constexpr float cameraSpeed = 10.f;
+    constexpr float cameraSensetivity = 5.f;
+
+    constexpr float rotSpeed = 360.f;
+}
