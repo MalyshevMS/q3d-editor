452179068ae013d4456ba78339e33cd4c1e5bc96
MalyshevMS <matvey.malyshev29@gmail.com>
2026-02-21 23:47:49 +0300

    добавил коробку и тест глубины

--- src/app.cpp
+++ src/app.cpp
@@ -1,7 +1,9 @@
+#include <glad/glad.h>
 #include "app.hpp"
 #include <q3d/gl/gl.hpp>
 #include <q3d/core/active_camera.hpp>
-#include <q3d/2d/plane.hpp>
+#include <q3d/obj/2d/plane.hpp>
+#include <q3d/obj/3d/box.hpp>
 #include <glm/glm.hpp>
 #include "config.txx"
 
@@ -14,15 +16,19 @@ Application::Application(std::string_view argv0)
 void Application::run() {
     auto texture = res->loadTexture("tex1", "res/texture.png");
     texture->setFilter(q3d::gl::Texture::Filter::NearestMMNearest, q3d::gl::Texture::Filter::Nearest);
-    texture->uv = glm::vec2(2, 2);
+    texture->uv = glm::vec2(1, 1);
 
     auto shader = res->loadShader("main", "res/main.vert", "res/main.frag");
 
-    q3d::q2d::Plane plane(shader, {}, texture);
+    q3d::object::Plane plane(shader, {}, texture);
+    q3d::object::Box box(shader, {}, texture);
+
+    box.transform.position.z = -5.f;
 
     cam.setPosition(glm::vec3(0.f, 0.f, 3.f));
 
     q3d::gl::clearColor(q3d::core::Color::Cyan);
+    q3d::gl::enable(q3d::gl::feature::depthTest);
     while (window.isOpen()) {
         // CPU (math)
 
@@ -32,7 +38,7 @@ void Application::run() {
         auto cameraMoveDelta = glm::vec3(0.f);
         auto cameraRotateDelta = glm::vec3(0.f);
 
-        plane.transform.rotation.z += cfg::rotSpeed * dt;
+        plane.transform.rotation.y += cfg::rotSpeed * dt;
 
         if (window.isKeyPressed(q3d::key::W)) {
             cameraMoveDelta.z += cameraMove;
@@ -66,6 +72,7 @@ void Application::run() {
         q3d::gl::clear();
         
         plane.draw();
+        box.draw();
 
         window.update();
     }
