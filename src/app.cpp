23dd8f5977d813a2b0ebcc4d4f758a764f27feb0
MalyshevMS <matvey.malyshev29@gmail.com>
2026-07-19 23:02:23 +0300

    начал внедрять FBO

--- src/app.cpp
+++ src/app.cpp
@@ -1,7 +1,9 @@
+#include <glad/glad.h>
 #include "app.hpp"
 #include "config.txx"
 #include <format>
 #include <q3d/q3d.hpp>
+#include <q3d/log/log.hpp>
 
 Application::Application(std::string_view argv0)
  : window("q3d editor", { 1280, 720 }), res(nullptr) {
@@ -19,6 +21,7 @@ void Application::run() {
 
     res->loadShader("object", "res/main.vert", "res/main.frag");
     res->loadShader("text", "res/text.vert", "res/text.frag");
+    res->loadShader("post", "res/post.vert", "res/post.frag");
     res->loadTexture("box", "res/box.png");
     res->loadModel("example", "res/example.obj", res->getShader("object"), res->getTexture("box"));
     res->loadFont("default", "res/font.ttf", 40);
@@ -41,10 +44,55 @@ void Application::run() {
         cam->setAspect(size.x / size.y);
     });
 
-    q3d::gl::clearColor(q3d::core::Color(0.5f, 0.5f, 0.5f));
 
     auto targetPos = cam->getPosition();
     auto targetRot = cam->getRotation();
+
+    unsigned int fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    unsigned int fboTex;
+    glGenTextures(1, &fboTex);
+    glBindTexture(GL_TEXTURE_2D, fboTex);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, window.getFBSize().x, window.getFBSize().y, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fboTex, 0);
+
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, window.getFBSize().x, window.getFBSize().y);
+
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        q3d::log::error("Something went wrong...");
+    }
+
+    const float vert[] = {
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+        1.0f,  1.0f,  1.0f, 1.0f
+    };
+
+    unsigned int vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vert), &vert, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 
     while (window.isOpen()) {
         // CPU (math)
@@ -100,9 +148,24 @@ Rotation: {5:.2f}; {6:.2f}; {7:.2f}
 
         // GPU
 
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+        q3d::gl::clearColor(q3d::core::Color::Gray);
         q3d::gl::clear();
 
         scene.render();
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        q3d::gl::disable(q3d::gl::feature::depthTest);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        res->getShader("post")->use();
+
+        glBindVertexArray(vao);
+        glBindTexture(GL_TEXTURE_2D, fboTex);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         canvas.render();
 
