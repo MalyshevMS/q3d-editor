f70014170ae46c07bfdbd61ebe1528ed0b772209
MalyshevMS <matvey.malyshev29@gmail.com>
2026-09-13 19:56:24 +0300

    тестирую сжатие zstd

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
-#include "app.hpp"
+#include <print>
+#include <q3d/res/vfs.hpp>
 
 int main(int argc, char const *argv[]) {
-    Application app;
-    app.run();
+    q3d::vfs::VFS vfs;
+
+    vfs.openArchive("res/pak001.q3d.tar.zst");
+
+    std::println("Files:");
+    for (auto i : vfs.listFiles()) {
+        std::println("{}", i);
+    }
+
+    std::println();
+
+    auto data = vfs.readFile("hello/hi.txt");
+
+    std::println("{}", data->asStringView());
 
     return 0;
 }
